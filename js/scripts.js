3d18a43f2c872c5c3d4b26fd079482dcee842860
LukeFarrell <luke.farrell@duke.edu>
2018-02-20 04:28:58 -0500

    with the weird envelopes

--- js/scripts.js
+++ js/scripts.js
@@ -17,10 +17,18 @@ allImages += '<img src="./img/friends/cooper.jpg" alt="pal">';
 
 $('#photos').append(allImages);
 
-var open=false
+$( "img" ).on( "mouseover", function() {
+      $( this ).css( "filter", "none" );
+});
+
+
+var open0=false
+
 $(document).ready(function(){
   $('.frame').click(function(){
-  $('.top').addClass('open');
+  $('.top0').addClass('open0');
+  $('.top1').addClass('open1');
+  $('.top2').addClass('open2');
 
   })
 });
@@ -28,44 +36,52 @@ $(document).ready(function(){
 
 var invis = false;
 $(window).scroll(function() {
-   var hT = $('#topId').offset().top,
-       hH = $('#topId').outerHeight(),
+   var hT = $('#topId0').offset().top,
+       hH = $('#topId0').outerHeight(),
        wH = $(window).height(),
        wS = $(this).scrollTop();
     // console.log((hT-wH) , wS);
     // console.log($('#sidebar').position())
     // console.log($('#topId').position())
 
-   if (wS > (hT+hH-wH)-300){
-     $("#button_open_envelope").click()
-     open=!open;
+   if (wS > (hT+hH-wH)){
+     $("#button_open_envelope0").click()
+     $("#button_open_envelope1").click()
+     $("#button_open_envelope2").click()
+
+     open0=!open0;
    }
 });
 
+
 $(window).scroll(function(){
   var windowTopPos = $(window).scrollTop();
-  var buttonPos = $('#button_open_envelope').offset().top;
-  var navBottomPos = $('#sidebar').offset().top + $('#sidebar').outerHeight();
-
+  var buttonPos = $('#button_open_envelope0').offset().top;
+  var start_trigger = $('#start_trigger').offset().top + 350;
+  var navBottomPos = $('#sidebar0').offset().top + $('#sidebar0').outerHeight();
 
 
   if(navBottomPos >= buttonPos) {
-    if(open){
-        $('.top').toggleClass('open');
-        open=false;
+    if(open0){
+        $('.top0').toggleClass('open0');
+        $('.top1').toggleClass('open1');
+        $('.top2').toggleClass('open2');
+
+        open0=false;
 
     }
   }
-  console.log($('#left').offset().top)
-  if(navBottomPos >= buttonPos){
-    $('#sidebar').css('visibility', 'hidden');
+  // console.log($('#left0').offset().top)
+  if(navBottomPos >= buttonPos || navBottomPos<=start_trigger){
+    $('#sidebar0').css('visibility', 'hidden');
+    $('#sidebar1').css('visibility', 'hidden');
+    $('#sidebar2').css('visibility', 'hidden');
+
     invis=true
   }
   else{
-    $('#sidebar').css('visibility', 'visible');
+    $('#sidebar0').css('visibility', 'visible');
+    $('#sidebar1').css('visibility', 'visible');
+    $('#sidebar2').css('visibility', 'visible');
   }
-});
-
-$( "img" ).on( "mouseover", function() {
-      $( this ).css( "filter", "none" );
 });
