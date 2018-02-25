31f0700c48d47f1549d1dbae2113532a34e1963e
LukeFarrell <luke.farrell@duke.edu>
2018-02-25 18:53:55 -0500

    magic intsall

--- js/scripts.js
+++ js/scripts.js
@@ -24,7 +24,7 @@ $( "img" ).on( "mouseover", function() {
 var open0=false
 
 $(document).ready(function(){
-  $('.frame').click(function(){
+  $('.frame0').click(function(){
   $('.top0').addClass('open0');
   $('.top1').addClass('open1');
   $('.top2').addClass('open2');
@@ -32,9 +32,19 @@ $(document).ready(function(){
   })
 });
 
+// document.getElementsByClassName('toggleButton')[0].onclick = function() {
+//   $('.top').toggleClass('open');
+//   open = !open;
+//   // console.log($('.top').attr('class'));
+
+
+
+// }
+
 
 var invis = false;
 $(window).scroll(function() {
+  // console.log(open0);
    var hT = $('#topId0').offset().top,
        hH = $('#topId0').outerHeight(),
        wH = $(window).height(),
@@ -42,39 +52,41 @@ $(window).scroll(function() {
     // console.log((hT-wH) , wS);
     // console.log($('#sidebar').position())
     // console.log($('#topId').position())
+    // console.log(wS,(hT+hH-wH))
 
    if (wS > (hT+hH-wH)){
      $("#button_open_envelope0").click()
      $("#button_open_envelope1").click()
      $("#button_open_envelope2").click()
-
+     // console.log("OPENING")
      open0=!open0;
    }
+
 });
 
 
 $(window).scroll(function(){
   var windowTopPos = $(window).scrollTop();
-  var start_trigger = $('#start_trigger').offset().top + 350;
   var buttonPos = $('#button_open_envelope0').offset().top;
-  var navBottomPos = $('#sidebar0').offset().top + $('#sidebar0').outerHeight();
-
+  var navBottomPos = $('#sidebar0').position().top + $('#sidebar0').offset().top;
+// console.log(navBottomPos,buttonPos);
 
   if(navBottomPos >= buttonPos) {
     if(open0){
         $('.top0').toggleClass('open0');
         $('.top1').toggleClass('open1');
         $('.top2').toggleClass('open2');
+     // console.log("CLOSING")
 
         open0=false;
 
     }
   }
   // console.log($('#left0').offset().top)
-  if(navBottomPos >= buttonPos || navBottomPos<=start_trigger){
-    $('#sidebar0').css('visibility', 'hidden');
-    $('#sidebar1').css('visibility', 'hidden');
-    $('#sidebar2').css('visibility', 'hidden');
+  if(navBottomPos >= buttonPos){
+    // $('#sidebar0').css('visibility', 'hidden');
+    // $('#sidebar1').css('visibility', 'hidden');
+    // $('#sidebar2').css('visibility', 'hidden');
 
     invis=true
   }
@@ -82,5 +94,108 @@ $(window).scroll(function(){
     $('#sidebar0').css('visibility', 'visible');
     $('#sidebar1').css('visibility', 'visible');
     $('#sidebar2').css('visibility', 'visible');
+
   }
+});
+var set=false;
+
+$(window).scroll(function() {
+   var hT = $('#start').offset().top,
+       wS = $(this).scrollTop();
+
+  if(wS > hT){
+    var height = $('#start').offset().top;
+
+
+      $('#sidebar0').css({
+          'position': 'fixed',
+          'left': '20%',
+          'bottom': '30%',
+          'transform': 'translate(-20%, 0)',
+          'width': '190px',
+          'min-height':'50px',
+          'height': 'auto',
+          'background': '#fff',
+          /*margin: auto;*/
+          'box-shadow': '0 0 5px 2px #333',
+          'z-index': '305'
+      });
+
+      $('#sidebar1').css({
+          'position': 'fixed',
+          'left': '50%',
+          'bottom': '30%',
+          'transform': 'translate(-50%, 0)',
+          'width': '190px',
+          'min-height':'50px',
+          'height': 'auto',
+          'background': '#fff',
+          /*margin: auto;*/
+          'box-shadow': '0 0 5px 2px #333',
+          'z-index': '305'
+
+      });
+
+      $('#sidebar2').css({
+          'position': 'fixed',
+          'left': '80%',
+          'bottom': '30%',
+          'transform': 'translate(-80%, 0)',
+          'width': '190px',
+          'min-height':'50px',
+          'height': 'auto',
+          'background': '#fff',
+          /*margin: auto;*/
+          'box-shadow': '0 0 5px 2px #333',
+          'z-index': '305'
+      });
+
+      set=true;
+  }
+
+//   else{
+
+//         $('#sidebar0').css({
+//           'position': 'relative',
+//           'left': '20%',
+//           'bottom': '30%',
+//           'transform': 'translate(-20%, 0)',
+//           'width': '190px',
+//           'min-height':'50px',
+//           'height': 'auto',
+//           'background': '#fff',
+//           /*margin: auto;*/
+//           'box-shadow': '0 0 5px 2px #333',
+//           'z-index': '305'
+//       });
+
+//       $('#sidebar1').css({
+//           'position': 'relative',
+//           'left': '20%',
+//           'bottom': '30%',
+//           'transform': 'translate(-20%, 0)',
+//           'width': '190px',
+//           'min-height':'50px',
+//           'height': 'auto',
+//           'background': '#fff',
+//           /*margin: auto;*/
+//           'box-shadow': '0 0 5px 2px #333',
+//           'z-index': '305'
+
+//       });
+
+//       $('#sidebar2').css({
+//           'position': 'relative',
+//           'left': '',
+//           'bottom': '',
+//           'transform': '',
+//           'width': '190px',
+//           'min-height':'50px',
+//           'height': 'auto',
+//           'background': '#fff',
+//           /*margin: auto;*/
+//           'box-shadow': '0 0 5px 2px #333',
+//           'z-index': '305'
+//       });
+//   }
 });
