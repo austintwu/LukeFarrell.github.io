26d291c8fe400d8ca9522d44b1c3c124bec3c6fe
LukeFarrell <luke.farrell@duke.edu>
2018-02-26 04:07:20 -0500

    testing letters

--- js/scripts.js
+++ js/scripts.js
@@ -21,181 +21,42 @@ $( "img" ).on( "mouseover", function() {
       $( this ).css( "filter", "none" );
 });
 
-var open0=false
+// init ScrollMagic Controller
+var controller = new ScrollMagic.Controller();
 
-$(document).ready(function(){
-  $('.frame0').click(function(){
-  $('.top0').addClass('open0');
-  $('.top1').addClass('open1');
-  $('.top2').addClass('open2');
+var envStartHeight=$('#memo1').offset().top;
+var memoStartHeight=$('#blue0').offset().top;
+// Scene Handler
 
-  })
-});
+// Scene2 Handler
+console.log(memoStartHeight-envStartHeight);
+var scene2 = new ScrollMagic.Scene({
+  triggerElement: "#pinned-trigger2", // point of execution
+  triggerHook:"onCenter",
+  duration: memoStartHeight-envStartHeight +25// pin the element for a total of 400px
 
-// document.getElementsByClassName('toggleButton')[0].onclick = function() {
-//   $('.top').toggleClass('open');
-//   open = !open;
-//   // console.log($('.top').attr('class'));
-
-
-
-// }
+})
+.setPin("#pinned-element2") // the element we want to pin
+.addTo(controller);
 
 
-var invis = false;
-$(window).scroll(function() {
-  // console.log(open0);
-   var hT = $('#topId0').offset().top,
-       hH = $('#topId0').outerHeight(),
-       wH = $(window).height(),
-       wS = $(this).scrollTop();
-    // console.log((hT-wH) , wS);
-    // console.log($('#sidebar').position())
-    // console.log($('#topId').position())
-    // console.log(wS,(hT+hH-wH))
+var scene3 = new ScrollMagic.Scene({
+  triggerElement: "#button_open_envelope0", // point of execution
+  triggerHook:.8
 
-   if (wS > (hT+hH-wH)){
-     $("#button_open_envelope0").click()
-     $("#button_open_envelope1").click()
-     $("#button_open_envelope2").click()
-     // console.log("OPENING")
-     open0=!open0;
-   }
+})
+.setClassToggle(".top0", "open0")
+// .setPin("#pinned-element2") // the element we want to pin
+.addTo(controller);
 
-});
+var scene3 = new ScrollMagic.Scene({
+  triggerElement: "#top0", // point of execution
+  triggerHook: 0.2
+})
+.setClassToggle(".top0", "close0")
+// .setPin("#pinned-element2") // the element we want to pin
+.addTo(controller);
 
 
-$(window).scroll(function(){
-  var windowTopPos = $(window).scrollTop();
-  var buttonPos = $('#button_open_envelope0').offset().top;
-  var navBottomPos = $('#sidebar0').position().top + $('#sidebar0').offset().top;
-// console.log(navBottomPos,buttonPos);
-
-  if(navBottomPos >= buttonPos) {
-    if(open0){
-        $('.top0').toggleClass('open0');
-        $('.top1').toggleClass('open1');
-        $('.top2').toggleClass('open2');
-     // console.log("CLOSING")
-
-        open0=false;
-
-    }
-  }
-  // console.log($('#left0').offset().top)
-  if(navBottomPos >= buttonPos){
-    // $('#sidebar0').css('visibility', 'hidden');
-    // $('#sidebar1').css('visibility', 'hidden');
-    // $('#sidebar2').css('visibility', 'hidden');
-
-    invis=true
-  }
-  else{
-    $('#sidebar0').css('visibility', 'visible');
-    $('#sidebar1').css('visibility', 'visible');
-    $('#sidebar2').css('visibility', 'visible');
-
-  }
-});
-var set=false;
-
-$(window).scroll(function() {
-   var hT = $('#start').offset().top,
-       wS = $(this).scrollTop();
-
-  if(wS > hT){
-    var height = $('#start').offset().top;
-
-
-      $('#sidebar0').css({
-          'position': 'fixed',
-          'left': '20%',
-          'bottom': '30%',
-          'transform': 'translate(-20%, 0)',
-          'width': '190px',
-          'min-height':'50px',
-          'height': 'auto',
-          'background': '#fff',
-          /*margin: auto;*/
-          'box-shadow': '0 0 5px 2px #333',
-          'z-index': '305'
-      });
-
-      $('#sidebar1').css({
-          'position': 'fixed',
-          'left': '50%',
-          'bottom': '30%',
-          'transform': 'translate(-50%, 0)',
-          'width': '190px',
-          'min-height':'50px',
-          'height': 'auto',
-          'background': '#fff',
-          /*margin: auto;*/
-          'box-shadow': '0 0 5px 2px #333',
-          'z-index': '305'
-
-      });
-
-      $('#sidebar2').css({
-          'position': 'fixed',
-          'left': '80%',
-          'bottom': '30%',
-          'transform': 'translate(-80%, 0)',
-          'width': '190px',
-          'min-height':'50px',
-          'height': 'auto',
-          'background': '#fff',
-          /*margin: auto;*/
-          'box-shadow': '0 0 5px 2px #333',
-          'z-index': '305'
-      });
-
-      set=true;
-  }
-
-//   else{
-
-//         $('#sidebar0').css({
-//           'position': 'relative',
-//           'left': '20%',
-//           'bottom': '30%',
-//           'transform': 'translate(-20%, 0)',
-//           'width': '190px',
-//           'min-height':'50px',
-//           'height': 'auto',
-//           'background': '#fff',
-//           /*margin: auto;*/
-//           'box-shadow': '0 0 5px 2px #333',
-//           'z-index': '305'
-//       });
-
-//       $('#sidebar1').css({
-//           'position': 'relative',
-//           'left': '20%',
-//           'bottom': '30%',
-//           'transform': 'translate(-20%, 0)',
-//           'width': '190px',
-//           'min-height':'50px',
-//           'height': 'auto',
-//           'background': '#fff',
-//           /*margin: auto;*/
-//           'box-shadow': '0 0 5px 2px #333',
-//           'z-index': '305'
-
-//       });
-
-//       $('#sidebar2').css({
-//           'position': 'relative',
-//           'left': '',
-//           'bottom': '',
-//           'transform': '',
-//           'width': '190px',
-//           'min-height':'50px',
-//           'height': 'auto',
-//           'background': '#fff',
-//           /*margin: auto;*/
-//           'box-shadow': '0 0 5px 2px #333',
-//           'z-index': '305'
-//       });
-//   }
-});
+// var tl = new TimelineMax();
+// tl.set($('#memo1'), {css:{zIndex:325}}) // set is basically a 0-second duration tween
